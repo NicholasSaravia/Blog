277f367c67756dd99e00507676e7a5643a61aae4
Nick Saravia <nick.saravia@gmail.com>
2021-02-23 15:47:19 -0500

    app-user & data-context

---
+++ D.L/Models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace D.L.Models
+{
+    public class AppUser : IdentityUser
+    {
+        [StringLength(20)]
+        public string DisplayName { get; set; }
+    }
+}
---
+++ P.L/Context/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using D.L.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace P.L.Context
+{
+    public class DataContext : IdentityDbContext<AppUser>
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+    }
+}
