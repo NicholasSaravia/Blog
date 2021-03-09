eec3e625fad5141c6ade7d740b1245a066922729
Nick Saravia <nick.saravia@gmail.com>
2021-03-09 09:15:02 -0500

    created migration to update appuserId on post from int to string

---
+++ P.L/Migrations/20210309141144_UpdateToPostModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P.L.Migrations
+{
+    public partial class UpdateToPostModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AppUserId",
+                table: "Posts",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AppUserId",
+                table: "Posts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+        }
+    }
+}
